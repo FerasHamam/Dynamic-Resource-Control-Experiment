e3c1b0a5e033f86f1221d6f1b61f8d1e0dbaa3a2
FerasHamam <feras.i.hamam@gmail.com>
2025-01-12 19:54:30 -0500

    added firewalld to setup scripts

--- InterferenceReceiver/scripts/setup.sh
+++ InterferenceReceiver/scripts/setup.sh
@@ -6,7 +6,7 @@ echo "Setup for Server started..."
 sudo apt-get update
 
 # Install required packages
-sudo apt-get install -y build-essential libzmq3-dev cmake pkg-config
+sudo apt-get install -y build-essential libzmq3-dev cmake pkg-config firewalld
 
 # Disable firewall in ports 5555 to 5560 in order for the Socket to communicate
 # Adjust based on the number of noise files that you are sending
