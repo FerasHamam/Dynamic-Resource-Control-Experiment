9c84e6a5be8758f08d987e4425340f0279114744
FerasHamam <feras.i.hamam@gmail.com>
2025-02-04 08:46:28 -0500

    Added termination for iperf3 noise

--- utils/noise/iperf3/kill_iperf3.sh
+++ utils/noise/iperf3/kill_iperf3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pgrep -f periodic_noise.sh | xargs kill && pkill iperf3
+pgrep -f run_periodic_noise_size.sh | xargs kill && pgrep -f run_periodic_sender.sh | xargs kill && pkill iperf3
