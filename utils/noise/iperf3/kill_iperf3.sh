3c232b37ae181cb2ebaf1f3f7197bd48954e7099
FerasHamam <feras.i.hamam@gmail.com>
2025-02-04 08:44:33 -0500

    Added tc script for One Layer Net

--- utils/noise/iperf3/kill_iperf3.sh
+++ utils/noise/iperf3/kill_iperf3.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 pgrep -f periodic_noise.sh | xargs kill && pkill iperf3
-
-./periodic_noise.sh 10.10.10.4 4444 1.1G 220 & ./periodic_noise.sh 10.10.10.4 4445 256M 70
-
-./periodic_noise.sh 10.10.10.8 4444 728M 150 & ./periodic_noise.sh 10.10.10.8 4445 128M 45
